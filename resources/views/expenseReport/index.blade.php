b9747d9fc2f4ec0bfa7ed2d77fc250695f4d6971
jcamargo <practicantedesarrollo2@solati.com.co>
2019-06-18 15:26:42 -0400

    clase layouts

--- resources/views/expenseReport/index.blade.php
+++ resources/views/expenseReport/index.blade.php
@@ -1,32 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Reports</title>
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="display-4 text-center">Reports</h1>
-                <table class="table table-hover table-bordered">
-                    <tr class="bg-info">
-                        <td>#</td>
-                        <td>Concepto</td>
-                    </tr>
-                    @foreach ($reports as $report)
-                    <tr>
-                        <td>{{$report->id}}</td>
-                        <td>{{$report->title}}</td>
-                    </tr>    
-                    @endforeach
-                    
-                </table>
-            </div>
-        </div>
+@extends('layouts.base')
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <h1 class="display-4 text-center">Reports</h1>
+        <table class="table table-hover table-bordered">
+            <tr class="bg-info">
+                <td>#</td>
+                <td>Concepto</td>
+            </tr>
+            @foreach ($reports as $report)
+            <tr>
+                <td>{{$report->id}}</td>
+                <td>{{$report->title}}</td>
+            </tr>    
+            @endforeach
+            
+        </table>
     </div>
-</body>
-</html>
+</div>
+@endsection
